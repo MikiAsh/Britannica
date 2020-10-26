53d927078081ad57d516954d3ff64663e214d349
Miki Ashkenazi <mikiash@gmail.com>
2020-10-26 22:34:27 +0200

    service: addPost & save methods

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Post } from '@app/models/Post'
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  posts = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  constructor() { }
+  posts: any = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  constructor() { 
+    window.localStorage.setItem('store', JSON.stringify({posts: this.posts}));
+  }
+
+  addPost(post: Post) {
+    this.posts = [...this.posts, post].sort((a: Post, b:Post) => (a.create_date > b.create_date) ? 1 : -1);
+    this.save()
+  }
+
+  save() {
+    window.localStorage.setItem('store', JSON.stringify({posts: this.posts}));
+  }
+
+
 }
