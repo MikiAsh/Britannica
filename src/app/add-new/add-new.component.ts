9f343d9555f9e196cf612db1e772d514a33b70d0
Miki Ashkenazi <mikiash@gmail.com>
2020-10-28 17:43:36 +0200

    add-new.component

---
+++ src/app/add-new/add-new.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { CreateNoteComponent } from '@app/create-note/create-note.component'
+
+@Component({
+  selector: 'app-add-new',
+  templateUrl: './add-new.component.html',
+  styleUrls: ['./add-new.component.scss']
+})
+export class AddNewComponent implements OnInit {
+
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit(): void {
+  }
+
+  popDialog(): void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '500px';
+    dialogConfig.height = '450px';
+
+    const dialogRef = this.dialog.open(CreateNoteComponent, dialogConfig);
+    // dialogRef.afterClosed().subscribe(result => {
+    //   console.log(`Dialog result: ${result}`);
+    // });
+  }
+}
